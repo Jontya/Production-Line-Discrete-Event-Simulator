3a63ad7b822747f9e31b08a8a35e0244f39c67aa
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-05-16 16:26:17 +1000

    Updated Main method

--- A3.java
+++ A3.java
@@ -1,8 +1,31 @@
+//---------------------------------------------------------------------------------------------------
+/** SENG2200 A3
+*** Jonty Atkinson (C3391110)
+*** 16/05/22
+***
+*** A3:
+*** 
+**/
+//---------------------------------------------------------------------------------------------------
+
 import java.util.*;
 import java.io.*;
 
 public class A3{
+
+    private static double M;
+    private static double N;
+    private static int QMax;
+
     public static void main (String args[]){
+        if(args.length == 3){
+            M = Double.parseDouble(args[0]);
+            N = Double.parseDouble(args[1]);
+            QMax = Integer.parseInt(args[2]);
+        }
+        else{
+            throw new ArrayIndexOutOfBoundsException("Invalid Paramaters");
+        }
         
     }
 }

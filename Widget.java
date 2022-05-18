569f2fdf718ad1b13f94935191a0fbfc29c2c1c6
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-05-18 14:33:13 +1000

    Create Widget.java

---
+++ Widget.java
@@ -0,0 +1,12 @@
+public class Widget {
+    
+    private String widgetID;
+
+    public Widget(String _widgetID){
+        widgetID = _widgetID;
+    }
+
+    public String getWidgetID(){
+        return widgetID;
+    }
+}

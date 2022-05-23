f0a0b709a8feb4f0642ef50eac5935e2fc0aa8c6
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-05-24 08:58:04 +1000

    Updated methods

--- MidStage.java
+++ MidStage.java
@@ -14,5 +14,17 @@ public class MidStage extends Stage {
 
         parallelStage = _parallelStage;
     }
+
+    public TimeEvent waiting(double currentTime){
+        return new TimeEvent(0);
+    }
+    
+    public void blocked(double currentTime){
+
+    }
+
+    public void starved(double currentTime){
+        
+    }
     
 }

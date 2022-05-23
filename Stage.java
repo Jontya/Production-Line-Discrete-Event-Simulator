c84ab8c7282ca002e0082e693999b5b1df8e4be4
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-05-23 16:49:02 +1000

    Added protected variables

--- Stage.java
+++ Stage.java
@@ -1,5 +1,13 @@
 public abstract class Stage {
 
-    
+    protected int M;
+    protected int N;
+    protected int parallelStage;
+
+    protected InterStageStorage<Widget> nextQueue;
+    protected InterStageStorage<Widget> prevQueue;
+
+    protected String ID;
+
     
 }

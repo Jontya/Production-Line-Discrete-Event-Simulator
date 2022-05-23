b9bb1597858180540da513fdcb107c9111f352d9
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-05-23 16:57:56 +1000

    Updated Constructors

--- FinalStage.java
+++ FinalStage.java
@@ -1,3 +1,15 @@
 public class FinalStage extends Stage{
+
+    public FinalStage(InterStageStorage<Widget> _prevQueue, String _ID){
+        prevQueue = _prevQueue;
+
+        ID = _ID;
+    }
+
+    public FinalStage(InterStageStorage<Widget> _prevQueue, String _ID, int _parallelStage){
+        this(_prevQueue, _ID);
+
+        parallelStage = _parallelStage;
+    }
     
 }
--- MidStage.java
+++ MidStage.java
@@ -1,3 +1,16 @@
 public class MidStage extends Stage {
+
+    public MidStage(InterStageStorage<Widget> _prevQueue, InterStageStorage<Widget> _nextQueue, String _ID){
+        prevQueue = _prevQueue;
+        nextQueue = _nextQueue;
+
+        ID = _ID;
+    }
+
+    public MidStage(InterStageStorage<Widget> _prevQueue, InterStageStorage<Widget> _nextQueue, String _ID, int _parallelStage){
+        this(_prevQueue, _nextQueue, _ID);
+
+        parallelStage = _parallelStage;
+    }
     
 }

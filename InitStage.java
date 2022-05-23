b637edbc2fc7d65217ab409c65971215d3c37992
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-05-23 16:49:30 +1000

    Added Constructors

--- InitStage.java
+++ InitStage.java
@@ -1,3 +1,17 @@
 public class InitStage extends Stage {
-    
+
+    public InitStage(InterStageStorage<Widget> _nextQueue, int _M, int _N, String _ID){
+        nextQueue = _nextQueue;
+
+        M = _M;
+        N = _N;
+
+        ID = _ID;
+    }
+
+    public InitStage(InterStageStorage<Widget> _nextQueue, int _M, int _N, String _ID, int _parallelStage){
+        this(_nextQueue, _M, _N, _ID);
+
+        parallelStage = _parallelStage;
+    }
 }

b2d4e837f670248ab50ac559f32e349343f1353f
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-05-24 15:52:50 +1000

    Update InitStage.java

--- InitStage.java
+++ InitStage.java
@@ -1,22 +1,29 @@
+import java.util.Random;
+
 public class InitStage extends Stage {
 
-    public InitStage(InterStageStorage<Widget> _nextQueue, int _M, int _N, String _ID){
+    public InitStage(InterStageStorage<Widget> _nextQueue, int _M, int _N, String _ID, Random _randomNum){
         nextQueue = _nextQueue;
+        randomNum = _randomNum;
 
         setProductionParameters(_M, _N);
         parallelStage = 1;
 
+        currentStatus = stageStatus.WAITING;
+
         ID = _ID;
     }
 
-    public InitStage(InterStageStorage<Widget> _nextQueue, int _M, int _N, String _ID, int _parallelStage){
-        this(_nextQueue, _M, _N, _ID);
+    public InitStage(InterStageStorage<Widget> _nextQueue, int _M, int _N, String _ID, Random _randomNum, int _parallelStage){
+        this(_nextQueue, _M, _N, _ID, _randomNum);
 
         parallelStage = _parallelStage;
     }
 
     public TimeEvent waiting(double currentTime){
-        return new TimeEvent(0);
+        widget = new Widget();
+        double processTime = getProcessingTime();
+        return new TimeEvent(processTime + currentTime);
     }
     
     public void blocked(double currentTime){

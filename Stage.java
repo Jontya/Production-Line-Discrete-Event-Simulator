0510b818db6999508f3612e884f6444f36c9361d
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-05-24 17:32:03 +1000

    Added a new status

--- Stage.java
+++ Stage.java
@@ -25,13 +25,17 @@ public abstract class Stage {
     protected stageStatus currentStatus;
 
     protected Widget widget;
-
     protected String ID;
 
+    protected double prevTime;
+    protected double timeBlocked;
+    protected double timeStarved;
+
     protected enum stageStatus{
-        WAITING,
+        IDLE,
         BLOCKED,
-        STARVED
+        STARVED,
+        OCCUPIED
     }
 
     protected void setProductionParameters(int _M, int _N){
@@ -40,7 +44,16 @@ public abstract class Stage {
     }
 
     protected double getProcessingTime(){
-        return (parallelStage * M) + (parallelStage * N) * ((randomNum.nextDouble() / 100) - 0.5);
+        randomNum = new Random(100);
+        double r = randomNum.nextDouble();
+        System.out.println(r);
+
+        randomNum = new Random(100);
+        r = randomNum.nextDouble();
+        System.out.println(r);
+
+
+        return (parallelStage * M) + (parallelStage * N) * (r - 0.5);
     }
 
     public String getID(){
@@ -48,7 +61,7 @@ public abstract class Stage {
     }
 
     public TimeEvent procesStage(double currentTime){
-        if(currentStatus == stageStatus.WAITING){
+        if(currentStatus == stageStatus.IDLE){
             return waiting(currentTime);
         }
         if(currentStatus == stageStatus.BLOCKED){
@@ -56,6 +69,9 @@ public abstract class Stage {
         }
         if(currentStatus == stageStatus.STARVED){
             starved(currentTime);
+        }
+        if(currentStatus == stageStatus.OCCUPIED){
+            occupied(currentTime);
         }
         return null;
     }
@@ -65,5 +81,7 @@ public abstract class Stage {
     public abstract void blocked(double currentTime);
 
     public abstract void starved(double currentTime);
+
+    public abstract void occupied(double currentTime);
 
 }

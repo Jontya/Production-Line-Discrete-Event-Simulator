050140c17af466166d37832af1892b8cb8668ede
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-05-24 15:53:22 +1000

    added process method

--- Stage.java
+++ Stage.java
@@ -15,26 +15,49 @@ public abstract class Stage {
     private int M;
     private int N;
 
+    protected Random randomNum;
+
     protected int parallelStage;
 
     protected InterStageStorage<Widget> nextQueue;
     protected InterStageStorage<Widget> prevQueue;
 
+    protected stageStatus currentStatus;
+
     protected Widget widget;
 
     protected String ID;
+
+    protected enum stageStatus{
+        WAITING,
+        BLOCKED,
+        STARVED
+    }
 
     protected void setProductionParameters(int _M, int _N){
         M = _M;
         N = _N;
     }
 
-    protected double getProcessingTime(Random randomNum){
-        return (parallelStage * M) + (parallelStage * N) * (randomNum.nextDouble() - 0.5);
+    protected double getProcessingTime(){
+        return (parallelStage * M) + (parallelStage * N) * ((randomNum.nextDouble() / 100) - 0.5);
     }
 
     public String getID(){
         return ID;
+    }
+
+    public TimeEvent procesStage(double currentTime){
+        if(currentStatus == stageStatus.WAITING){
+            return waiting(currentTime);
+        }
+        if(currentStatus == stageStatus.BLOCKED){
+            blocked(currentTime);
+        }
+        if(currentStatus == stageStatus.STARVED){
+            starved(currentTime);
+        }
+        return null;
     }
 
     public abstract TimeEvent waiting(double currentTime);

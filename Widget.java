42e41d497a638948a9786451955e4b4eb2c21c03
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-05-24 17:32:12 +1000

    Extra storage variables

--- Widget.java
+++ Widget.java
@@ -1,12 +1,27 @@
+import java.util.*;
+
 public class Widget {
     
     private String widgetID;
+    private double startTime;
+    private double endTime;
 
-    public Widget(){
+    public Widget(double _startTime, double _endTime){
         widgetID = IDGenerator.getInstance().getID();
+
+        startTime = _startTime;
+        endTime = _endTime;
     }
 
     public String getWidgetID(){
         return widgetID;
+    }
+
+    public double getStartTime(){
+        return startTime;
+    }
+
+    public double getEndTime(){
+        return endTime;
     }
 }

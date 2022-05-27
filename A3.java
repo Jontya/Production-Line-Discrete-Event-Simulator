9b890dca9d195c68738abb3d17d4a1b8a79c2ec8
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-05-27 16:51:00 +1000

    Updated Param Check

--- A3.java
+++ A3.java
@@ -1,25 +1,23 @@
 //---------------------------------------------------------------------------------------------------
 /** SENG2200 A3
 *** Jonty Atkinson (C3391110)
-*** 16/05/22
+*** 20/05/22
 ***
 *** A3:
-*** 
+*** Entry point for the program, used to check CLI parameters and begin production
 **/
 //---------------------------------------------------------------------------------------------------
 
-import java.util.*;
-import java.io.*;
-
 public class A3{
-
-    public static void main (String args[]){
+    public static void main(String[] args){
+        // Checks for correct number of CLI parameters
         if(args.length == 3){
-            ProductionLine productionLine = new ProductionLine(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]));
-            productionLine.beginProduction();
+            ProductionLine productionLine = new ProductionLine(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2])); // Creates a new production line
+            productionLine.beginProduction(); // Begins production
+            System.out.println(productionLine.getReport()); // Prints report
         }
         else{
-            throw new ArrayIndexOutOfBoundsException("Invalid Paramaters");
+            throw new ArrayIndexOutOfBoundsException("Invalid Parameters");
         }
     }
 }

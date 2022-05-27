10c3cec10a413d43c1d4c2fbbe8573df1db9e2c8
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-05-27 16:53:19 +1000

    Updated storage methods

--- Widget.java
+++ Widget.java
@@ -1,27 +1,45 @@
-import java.util.*;
+//---------------------------------------------------------------------------------------------------
+/** SENG2200 A3
+*** Jonty Atkinson (C3391110)
+*** 23/05/22
+***
+*** Widget:
+*** basic widget class that is used to store a list of simulation events (used in the report) and a
+*** globally unique widget ID. Widgets are stored in the inter-stage storage queues and also used
+*** in each stage.
+**/
+//---------------------------------------------------------------------------------------------------
+
+import java.util.ArrayList;
 
 public class Widget {
-    
+
+    private ArrayList<SimulationEvent> simulationEvents;
     private String widgetID;
-    private double startTime;
-    private double endTime;
 
-    public Widget(double _startTime, double _endTime){
-        widgetID = IDGenerator.getInstance().getID();
-
-        startTime = _startTime;
-        endTime = _endTime;
+    // Constructor
+    public Widget(String _widgetID){
+        simulationEvents = new ArrayList<>();
+        widgetID = _widgetID;
     }
 
-    public String getWidgetID(){
+    // Gets widget ID
+    public String getID(){
         return widgetID;
     }
 
-    public double getStartTime(){
-        return startTime;
+    // Adds new event to the list
+    public void addEvent(double _startTime, double _endTime, String _stageID){
+        simulationEvents.add(new SimulationEvent(_startTime, _endTime, _stageID));
     }
 
-    public double getEndTime(){
-        return endTime;
+    // Gets the last end time in the list
+    public double getLastEndTime(){
+        return simulationEvents.get(simulationEvents.size() - 1).getEndTime();
     }
+
+    public ArrayList<SimulationEvent> getSimEvents(){
+        return simulationEvents;
+    }
+    
 }

9d9f083c5627722abe24c05bd5980fe6aba1f843
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-06-01 16:26:22 +1000

    Prints the report after production

--- A3.java
+++ A3.java
@@ -12,9 +12,9 @@ public class A3{
     public static void main(String[] args){
         // Checks for correct number of CLI parameters
         if(args.length == 3){
-            ProductionLine productionLine = new ProductionLine(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2])); // Creates a new production line
-            productionLine.beginProduction(); // Begins production
-            System.out.println(productionLine.getReport()); // Prints report
+            ProductionLine productionLine = new ProductionLine(); // Creates a new production line
+            productionLine.beginProduction(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2])); // Begins production
+            System.out.println(productionLine.productionReport());
         }
         else{
             throw new ArrayIndexOutOfBoundsException("Invalid Parameters");

568582d23dfafa539f37cc394956d9f8c37503b4
Jonty Atkinson <47051870+Jontya@users.noreply.github.com>
2022-07-13 23:53:31 +1000

    Update SimulationEvent.java

--- SimulationEvent.java
+++ SimulationEvent.java
@@ -1,13 +1,3 @@
-//---------------------------------------------------------------------------------------------------
-/** SENG2200 A3
-*** Jonty Atkinson (C3391110)
-*** 23/05/22
-***
-*** SimulationEvent:
-*** Stored in a list inside of a widget class. Only used to calculate stats in the report
-**/
-//---------------------------------------------------------------------------------------------------
-
 public class SimulationEvent {
 
     private double startTime;
